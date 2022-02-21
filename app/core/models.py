2b89761f3882e12fcd27246422242e94eaed2b75
Máximo Fernández Arenas <Max@Maximos-MacBook-Pro.local>
2022-02-21 14:42:51 -0300

    checks that a mail is not null

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
+        if not email: 
+            raise ValueError("Invalid Email: Must not be undefined.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
 
@@ -22,4 +23,10 @@ class ModelTests(TestCase):
         user = get_user_model().object.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating User with no email raises error"""
+        email = None
+        with self.assertRaises(ValueError):
+            get_user_model().object.create_user(email, 'test123')
 
