bdb34c60b90c2be9c883862b564a35ee95bb120a
maxfer03 <sandsferdinand@gmail.com>
2022-03-22 15:32:49 -0300

    added image field to recipe model

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 #from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('api/recipe/', include('recipe.urls')),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from operator import truediv
 from os import link
 from django.db import models
@@ -6,6 +8,12 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 from django.conf import settings
 
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/',filename)
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -79,7 +87,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
-    
     def __str__(self):
         return self.title
